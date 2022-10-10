c1b2f298889f5e77315663edd7c17a34bf9b4b5d
Wellingto-well <113539794+Wellingto-well@users.noreply.github.com>
2022-10-10 17:19:55 -0300

    Update aposta com dados.py

--- aposta com dados.py
+++ aposta com dados.py
@@ -12,9 +12,9 @@ for aposta in range(0,1000):
     soma_dos_dados = dado1 + dado2
     print('EXPLICANDO O JOGO')
     print('')
-    print(f'Vamos sortear 2 dados e soma-los se der 7 ou 2 voce ganhar o dobro dq aposto')
+    print(f'Vamos sortear 2 dados e soma-los se der 7 ou 2 voce ganha o dobro do que aposto')
     print(f'Se der 6 voce perde o que aposto')
-    print(f'Se der 10 vai empatar e vc nao perde e nem ganha nada')
+    print(f'Se der 10 vai empatar e voce não perde e nem ganha nada')
     print(f'Se nao der nenhum desses resultado vc tera oportunidade de tentar novamente')
     print(f'')
     
@@ -43,7 +43,7 @@ for aposta in range(0,1000):
                 break
 
         elif soma_dos_dados == 6 or soma_dos_dados == 5:
-            print(f'Que pena voce perdeu o resultado do dado foi {soma_dos_dados}')
+            print(f'Que pena voce perdeu o resultado do dado foi: {soma_dos_dados}')
             total_derrota =+ 1
             novo_saldo_derrota = 0 - valor_da_aposta  
             derrota.append(novo_saldo_derrota)
@@ -55,7 +55,7 @@ for aposta in range(0,1000):
                 break
 
         elif soma_dos_dados == 10:
-            print(f'Empatou o resultado do dado foi {soma_dos_dados}') 
+            print(f'Empatou o resultado do dado foi: {soma_dos_dados}') 
 
             tentar_novamente_empate = int(input('Se quiser tentar novamente digite 1 caso \nSe nao quiser tentar novamente digite 2: '))
             if tentar_novamente_empate == 1:
@@ -64,7 +64,7 @@ for aposta in range(0,1000):
                 break
 
         else:
-            print(f'Quer tentar denovo? O resultado do dado foi {soma_dos_dados}')
+            print(f'Quer tentar denovo? O resultado do dado foi: {soma_dos_dados}')
 
             tentar_novamente = int(input('Se quiser tentar novamente digite 1 caso \nSe nao quiser tentar novamente digite 2: '))
 
@@ -73,7 +73,7 @@ for aposta in range(0,1000):
             else:
                 break
     else:
-        print(f'O valor da aposta é maior que o saldo. Esse é seu saldo{saldo}')
+        print(f'O valor da aposta é maior que o saldo. Esse é seu saldo: {saldo}')
 
     somas_das_vitoria = sum(vitoria)
     somas_das_derrotas = sum(derrota)
